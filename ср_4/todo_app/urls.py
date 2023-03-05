8e389c02f6c48b6663ead15ee0c9d1526599e516
OmarovaNazira <123797768+OmarovaNazira@users.noreply.github.com>
2023-03-05 21:40:38 +0600

    Update urls.py

--- ср_4/todo_app/urls.py
+++ ср_4/todo_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from online_shop import views
+from todo_app import views
 
 urlpatterns = [
     path('', views.home_page),
     path('categories/<int:pk>', views.category_page),
     path('about', views.about_page),
-    path('online-shop/categories', views.categories_handler),
-    path('online-shop/categories/<int:pk>', views.category_handler),
-    path('online-shop/categories/<int:pk>/products', views.category_products_handler),
-    path('online-shop/products', views.products_handler),
-    path('online-shop/products/<int:pk>', views.product_handler)
+    path('todo_app/categories', views.categories_handler),
+    path('todo_app/categories/<int:pk>', views.category_handler),
+    path('todo_app/categories/<int:pk>/products', views.category_products_handler),
+    path('todo_app/products', views.products_handler),
+    path('todo_app/products/<int:pk>', views.product_handler)
 ]
