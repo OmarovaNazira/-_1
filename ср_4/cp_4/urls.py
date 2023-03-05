52d7aabeaa43b420b61820f628171b48a191a21c
OmarovaNazira <123797768+OmarovaNazira@users.noreply.github.com>
2023-03-05 21:45:12 +0600

    Update urls.py

--- ср_4/cp_4/urls.py
+++ ср_4/cp_4/urls.py
@@ -1,4 +1,4 @@
-"""cp_3 URL Configuration
+"""cp_4 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
