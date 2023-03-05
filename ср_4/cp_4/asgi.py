dbd3e5aeb5a04fcc3d423080d14846f79035b60c
OmarovaNazira <123797768+OmarovaNazira@users.noreply.github.com>
2023-03-05 21:43:31 +0600

    Update asgi.py

--- ср_4/cp_4/asgi.py
+++ ср_4/cp_4/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for cp_3 project.
+ASGI config for cp_4 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cp_3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cp_4.settings')
 
 application = get_asgi_application()
